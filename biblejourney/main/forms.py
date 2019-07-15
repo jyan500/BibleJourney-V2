01f71e26b05888ecee5e36a477b6ea23edc6a59e
Jansen Yan <jyan500@gmail.com>
2019-07-14 17:37:24 -0700

    added the api call to bible-api to retrieve verses

--- biblejourney/main/forms.py
+++ biblejourney/main/forms.py
@@ -1,24 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-class RegistrationForm(FlaskForm):
-	username = StringField('Username', 
-		validators = [DataRequired(), Length(min=2, max=20)])
-	email = StringField('Email', validators=[DataRequired(), Email()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
-	submit = SubmitField('Sign Up')
+class VersesForm(FlaskForm):
+	verse = StringField('Verse', 
+		validators = [DataRequired()])
+	submit = SubmitField('Search')
 
-	def validate_username(self, username):
+	# def validate_username(self, username):
 
-		user = User.query.filter_by(username=username.data).first()
-		if user:
-			raise ValidationError('That username is not available, please choose another username!')
-	def validate_email(self, email):
-		email = User.query.filter_by(email=email.data).first()
-		if email:
-			raise ValidationError('That email is not available, please choose another email!')
+	# 	user = User.query.filter_by(username=username.data).first()
+	# 	if user:
+	# 		raise ValidationError('That username is not available, please choose another username!')
+	# def validate_email(self, email):
+	# 	email = User.query.filter_by(email=email.data).first()
+	# 	if email:
+	# 		raise ValidationError('That email is not available, please choose another email!')
 
 
 
