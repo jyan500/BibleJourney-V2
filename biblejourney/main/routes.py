96d78590d53a7bf12f7cf68f1f0ec8571648f78f
Jansen Yan <jyan500@gmail.com>
2019-07-24 20:09:09 -0700

    Added book reference table to get the amount of chapters for each book

--- biblejourney/main/routes.py
+++ biblejourney/main/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, Blueprint, flash
 from biblejourney.main.forms import VersesForm 
+from biblejourney.models import BookRef 
 import json
 import requests
 import sys
@@ -15,6 +16,31 @@ def home():
 @main.route("/about")
 def about():
 	return render_template('main/about.html', title = 'About')
+
+@main.route("/verses/<string:search_param>")
+def verses_links(search_param):
+	regex = re.compile('^(\\d?\\s?[a-zA-Z]+)(\\s\\d+)([:]?\\d+)?([-]?\\d+)?$')
+	form = VersesForm(request.args)
+	is_only_chapter = True
+	match = regex.match(search_param)
+	if (match):
+		match_groups = match.groups()
+		book = match_groups[0]
+		chapter = match_groups[1]
+		start_verse = match_groups[2]
+		end_verse = match_groups[3]
+		if (start_verse or end_verse):
+			is_only_chapter = False
+	else:
+		flash("Please search with one of the following formats: John 3, John 3:16, John 3:16-19", "danger")
+		return render_template("main/home.html", form=form)
+	json_result = getVerseBodyRequest(search_param)
+	if (json_result.get('error')):
+		flash("Verses could not be found!", "danger")
+		return render_template("main/home.html", form=form)
+	else:
+		return render_template("main/home.html", form=form, verses=json_result, book = book, chapter = chapter, is_only_chapter = is_only_chapter)
+
 
 @main.route("/verses")
 def verses():
@@ -42,9 +68,7 @@ def verses():
 		flash("Verses could not be found!", "danger")
 		return render_template("main/home.html", form=form)
 	else:
-		print('here', file = sys.stderr)
-		return render_template("main/home.html", form=form, verses=json_result, is_only_chapter = is_only_chapter)
-
+		return render_template("main/home.html", form=form, verses=json_result, book = book, chapter = chapter, is_only_chapter = is_only_chapter)
 	## version is world english bible by default until different versions are supported
 def getVerseBodyRequest(param: str):
 	## if start verse and end verse are provided
--- biblejourney/models.py
+++ biblejourney/models.py
@@ -19,3 +19,11 @@ class User(db.Model, UserMixin):
 
 	def __repr__(self):
 		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class BookRef(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	book = db.Column(db.String(20), unique=True, nullable=False)
+	num_chapters = db.Column(db.String(20), nullable=False)
+
+	def __repr__(self):
+		return f"BookRef('{self.book}', '{self.num_chapters}')"
